55738a7e0ac330ef0bda96ef2bcd1a4b00955d71
Park Hyeon gi <hg020@naver.com>
2025-07-01 17:50:40 +0900

    0701 / 박현기 수업 커밋

---
+++ sp02/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -0,0 +1,22 @@
+package com.yedam.app.emp.service;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder // 생성할려고 하는 클래스에게 넘겨줄려는 
+@AllArgsConstructor
+@NoArgsConstructor // @Builder 시 생략되므로 명시적으로 추가
+public class EmpVO {
+	
+	private Integer employeeId;
+	private String lastName;
+	private String email;
+	private Date hireDate;
+	private String jobId;
+	private double salary;
+}
--- sp02/src/test/java/com/yedam/app/Sp02ApplicationTests.java
+++ sp02/src/test/java/com/yedam/app/Sp02ApplicationTests.java
@@ -1,13 +1,57 @@
 package com.yedam.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
 
 @SpringBootTest
 class Sp02ApplicationTests {
-
+	
+	@Autowired // 필드 주입 : 테스트용, private로 지정된걸 강제로 필드주입 하기때문에 사용을 권장하지 않는다
+	private EmpMapper empMapper;
+	
+	//@Test // 해당 메서드를 테스트하겠다고 선언
+	public void selectAll() {
+		List<EmpVO> list = empMapper.selectAll();
+		for(EmpVO emp : list) {
+			System.out.println(emp.getLastName());
+		}
+		assertTrue(!list.isEmpty()); //단정 메서드, junit이 제공하는 메서드, 
+	}
+	
 	@Test
-	void contextLoads() {
+	public void selectOne() {
+		EmpVO emp = EmpVO.builder().employeeId(100).build(); // Builder 역할 : instance 생성, field init 초기화 
+		
+		EmpVO findVO = empMapper.selectInfo(emp);
+		System.out.println(findVO);
+		
+		assertEquals("King", findVO.getLastName()); // "내가 원하는값", 실행했을때 나오는 비교값 -> 같을때 성공을 의미
+	}
+	
+	//@Test
+	public void insertValue() {
+		EmpVO emp = EmpVO.builder().employeeId(1000).lastName("Kang").email("kg@google.com").jobId("SA_REP").build();
+		
+		int result = empMapper.insertInfo(emp);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void insertSelectKey() {
+		EmpVO emp = EmpVO.builder().lastName("Hong").email("hong@naver.com").jobId("IT_PROG").salary(1200).build();
+		int result = empMapper.insertInfo(emp);
+		System.out.println("사원번호 : " + emp.getEmployeeId());
+		
+		assertEquals(1, result);
 	}
 
 }
