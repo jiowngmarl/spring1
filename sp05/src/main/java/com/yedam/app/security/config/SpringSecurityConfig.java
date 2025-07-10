197ca8667dad03274d4da3bd768a5e652047a06e
Park Hyeon gi <hg020@naver.com>
2025-07-10 17:54:04 +0900

    0710 / Spring 개발환경 설정

--- sp05/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
+++ sp05/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
@@ -2,23 +2,67 @@ package com.yedam.app.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
+
+import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
-	
+
+//    private final SecurityFilterChain filterChain;
+//
+//    SpringSecurityConfig(SecurityFilterChain filterChain) {
+//        this.filterChain = filterChain;
+//    }
 	@Bean // 비밀번호 암호화 : 단방향
 	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();	
 	}
 	
-	@Bean // 메모리상 인증정보 등록 => 테스트 전용 방식
+	//인증 및 인가 : Lambda DSL
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http //Security가 적용될 URI / 핵심
+			.authorizeHttpRequests( authrize
+					-> authrize
+					  .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+					  .requestMatchers("/","/all").permitAll()
+					  .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+					  .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+					  .anyRequest().authenticated())
+			.formLogin(formLogin // 필수
+					-> formLogin
+					.usernameParameter("id")
+					.defaultSuccessUrl("/all"))
+			.logout(logout   // 필수
+					-> logout
+					.logoutSuccessUrl("/all")
+					.invalidateHttpSession(true)
+					.deleteCookies("JSESSIONID"));
+		
+		//http.csrf(csrf -> csrf.disable());
+		return http.build();
+	}
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web ->
+				web
+					.ignoring()
+					.requestMatchers("/images/**", "/js/**", "/css/**");
+	}
+/*	
+	@Bean //메모리상 인증정보 등록 => 테스트 전용 방식
 	InMemoryUserDetailsManager inMemoryUserDetailsService() {
 		UserDetails user = User.builder()
 							   .username("user1")
@@ -27,11 +71,16 @@ public class SpringSecurityConfig {
 							   .build();
 		
 		UserDetails admin = User.builder()
-								.username("admin1")
-								.password(passwordEncoder().encode("1234"))
-								.authorities("ROLE_ADMIN", "BOLE_USER")
-								.build();
+							    .username("admin1")
+							    .password(passwordEncoder().encode("1234"))
+							    .authorities("ROLE_ADMIN", "ROLE_USER")
+							    .build();
 		
 		return new InMemoryUserDetailsManager(user, admin);
 	}
+*/
 }
+	
+
+	
+
---
+++ sp05/src/main/java/com/yedam/app/security/web/SampleController.java
@@ -0,0 +1,33 @@
+package com.yedam.app.security.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class SampleController {
+	
+	@GetMapping("all")
+	public void all() {
+		
+	}
+	
+	@GetMapping("user")
+	public void user() {
+		
+	}
+	
+	@GetMapping("admin")
+	public void admin() {
+		
+	}
+	
+	@PostMapping("info")
+	@ResponseBody
+	public String info(String keyword) {
+		return "Server Response : " + keyword;
+	}
+}
